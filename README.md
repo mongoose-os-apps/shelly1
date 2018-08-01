1cd8e9a950abf09ceaa66fd596bee6109931d8b6
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2018-08-01 14:56:00 +0300

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Application for Shelly1 by Allterco
+
+## Overview
+
+This application bundles useful libraries and enables programming in JavaScript
+for the Shelly1 WiFi-controlled relay.
+
+## How to install this app
+
+- Install and start [mos tool](https://mongoose-os.com/software.html)
+- Switch to the Project page, find and import this app, build and flash it:
+
+<p align="center">
+  <img src="https://mongoose-os.com/images/app1.gif" width="75%">
+</p>
---
+++ fs/init.js
@@ -0,0 +1,80 @@
+load('api_config.js');
+load('api_events.js');
+load('api_gpio.js');
+load('api_mqtt.js');
+load('api_net.js');
+load('api_sys.js');
+load('api_timer.js');
+
+let relay_pin = Cfg.get('pins.relay');
+let sw_pin = Cfg.get('pins.sw');
+let pub_topic = '/shelly1/' + Cfg.get('device.id') + '/events';
+let sub_topic = '/shelly1/' + Cfg.get('device.id') + '/commands';
+
+print('Relay GPIO:', relay_pin, ' SW GPIO:', sw_pin);
+
+let getInfo = function() {
+  return JSON.stringify({
+    total_ram: Sys.total_ram(),
+    free_ram: Sys.free_ram(),
+    relay_on: GPIO.read(relay_pin),
+    uptime: Sys.uptime()
+  });
+};
+
+function publishStatus() {
+  let message = getInfo();
+  let ok = MQTT.pub(pub_topic, message, 1);
+  print('Published:', ok, pub_topic, '->', message);
+}
+
+// Keep relay OFF by default
+GPIO.set_mode(relay_pin, GPIO.MODE_OUTPUT);
+GPIO.write(relay_pin, 0);
+
+// Configure SW pin
+GPIO.set_mode(sw_pin, GPIO.MODE_INPUT);
+GPIO.set_pull(sw_pin, GPIO.PULL_NONE);
+
+// Each button press toggles the relay and sends an update
+GPIO.set_button_handler(sw_pin, GPIO.PULL_NONE, GPIO.INT_EDGE_POS, 100, function() {
+  GPIO.toggle(relay_pin);
+  publishStatus();
+}, null);
+
+// Subscribe for commands over MQTT
+MQTT.sub(sub_topic, function(conn, topic, msg) {
+  if (msg == 'on') {
+    GPIO.write(relay_pin, 1);
+  } else if (msg == 'off') {
+    GPIO.write(relay_pin, 0);
+  } else if (msg == 'toggle') {
+    GPIO.toggle(relay_pin);
+  } else {
+    print("unrecognized command");
+    return;
+  }
+  publishStatus();
+}, null);
+
+// Publish our status 
+MQTT.setEventHandler(function(conn, ev, edata) {
+  if (ev == MQTT.EV_CONNACK) {
+    publishStatus();
+  }
+});
+
+// Monitor network connectivity.
+Event.addGroupHandler(Net.EVENT_GRP, function(ev, evdata, arg) {
+  let evs = '???';
+  if (ev === Net.STATUS_DISCONNECTED) {
+    evs = 'DISCONNECTED';
+  } else if (ev === Net.STATUS_CONNECTING) {
+    evs = 'CONNECTING';
+  } else if (ev === Net.STATUS_CONNECTED) {
+    evs = 'CONNECTED';
+  } else if (ev === Net.STATUS_GOT_IP) {
+    evs = 'GOT_IP';
+  }
+  print('== Net event:', ev, evs);
+}, null);
